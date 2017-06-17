84e4aa5a724649dfde4728de3bfcc92a77a0967f
kingdom <kingdom>
2017-06-17 16:00:29 +0800

    deleted:    data/brush_hair_test_split1.txt 	deleted:    data/brush_hair_test_split2.txt 	deleted:    data/brush_hair_test_split3.txt 	deleted:    data/dive_test_split1.txt 	deleted:    data/dive_test_split2.txt 	deleted:    data/dive_test_split3.txt 	deleted:    data/shake_hands_test_split1.txt 	deleted:    data/shake_hands_test_split2.txt 	deleted:    data/shake_hands_test_split3.txt 	modified:   src/cluster.py

--- src/cluster.py
+++ src/cluster.py
@@ -37,13 +37,13 @@ if __name__ == '__main__':
     # flag: {0：not used, 1:train, 2:test}
     round = 1 
     flag = 1
-    x_rand = np.load('../data/stips_r%d_f%d'%(round,flag))
+    x_rand = np.load('../data/stips_r%d_f%d.npy'%(round,flag))
 
     K = 128
-    kms = sklCluster.KMeans(n_clusters=K, n_jobs=1, random_state=0).fit(x_rand)
+    kms = sklCluster.KMeans(n_clusters=K, n_jobs=-1, random_state=0).fit(x_rand)
 
     joblib.dump(kms, '../data/kmeans_r%d_f%d_k%d.model'%(round, flag, K), compress=3)
     # kms = joblib.load(kms, '../data/kmeans_r%d_f%d_k%d.model'%(round, flag, K))
 
     np.save('../data/kmeans_r%d_f%d_k%d.model'%(round, flag, K), kms.cluster_centers_)
-    centroids = np.load('../data/kmeans_r%d_f%d_k%d.model'%(round, flag, K))
+    # centroids = np.load('../data/kmeans_r%d_f%d_k%d.model'%(round, flag, K))
